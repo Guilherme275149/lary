8cd3baecd6023d179042dc3a24a06316635cc7e0
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-03-27 16:59:05 -0300

    Add files via upload

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  𝑮𝒐𝒘𝒕𝒆𝒓
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* 🐊🚩`
+}
+exports.cekvip = cekvip
---
+++ src/daftarvip.js
@@ -0,0 +1,15 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DO PREMIUM:*
+
+-1. 10 > Acessar recursos premium
+-2. 40 > Recursos vip + base do bot editável!
+
+*SE QUER TER PREMIUM:*
+
+*CHAME O 𝑮𝒐𝒘𝒕𝒆𝒓:*
+
+_wa.me/5511934488829 ou digite *${prefix}owner*_`
+}
+exports.daftarvip = daftarvip
